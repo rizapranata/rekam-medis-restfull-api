84bb657ce172afd4bf73f409bc1dfa2a577ea8e6
Riza Pranata <riza.pranata@appschef.com>
2023-09-26 23:47:35 +0700

    added feature login super user

--- src/application/web.js
+++ src/application/web.js
@@ -3,7 +3,7 @@ import { publicRouter } from "../route/public-api.js";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 
 export const web = express();
-
 web.use(express.json());
+
 web.use(publicRouter);
 web.use(errorMiddleware)
--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -4,9 +4,14 @@ const registerUserValidation = Joi.object({
     super_user: Joi.string().max(100).required(),
     password: Joi.string().max(100).required(),
     name: Joi.string().max(100).required(),
-
 });
 
+const loginUserValidation = Joi.object({
+    super_user: Joi.string().max(100).required(),
+    password: Joi.string().max(100).required()
+})
+
 export {
-    registerUserValidation
+    registerUserValidation,
+    loginUserValidation
 }
